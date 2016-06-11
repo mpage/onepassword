68d725d1be379247cb1a4e10e85ba5861395231f
Matt Page <mpage@brkt.com>
2016-06-10 21:13:14 -0700

    Add doc.go

---
+++ doc.go
@@ -0,0 +1,13 @@
+/*
+Package onepassword provides read-only access to items stored in the 1Password SQLite database.
+
+
+
+Compatibility
+
+This package has been tested against a version of 1Password 6 installed from
+the App Store. In theory, it should work against any version of 1Password that
+uses the OPVault data format to store items in a SQLite database. In practice,
+this means versions of 1Password >= 5. YMMV.
+*/
+package onepassword
